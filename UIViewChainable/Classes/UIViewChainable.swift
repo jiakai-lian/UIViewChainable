9bc1b1493c6af74f671428cfa4d83a7027687988
Jacky Lian <jacky.lian@fleetcutter.com>
2017-11-29 08:52:55 +1100

    * Make methods public

--- UIViewChainable/Classes/UIViewChainable.swift
+++ UIViewChainable/Classes/UIViewChainable.swift
@@ -9,20 +9,20 @@ import UIKit
 public protocol UIViewChainable {}
 public extension UIViewChainable where Self: UIView {
     @discardableResult
-    func config(_ config: (Self) -> Void) -> Self {
+    public func config(_ config: (Self) -> Void) -> Self {
         config(self)
         return self
     }
     
     @discardableResult
-    func layout(_ layout: (Self) -> Void) -> Self {
+    public func layout(_ layout: (Self) -> Void) -> Self {
         layout(self)
         return self
     }
 }
 
 extension UIView: UIViewChainable {
-    func attachTo(_ superView: UIView) -> Self {
+    public func attachTo(_ superView: UIView) -> Self {
         superView.addSubview(self)
         return self
     }
