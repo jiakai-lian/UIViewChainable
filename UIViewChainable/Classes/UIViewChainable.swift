0d64f996c3d19056988eaf327f95398f2c07317d
Jacky Lian <jacky.lian@fleetcutter.com>
2017-12-19 15:51:36 +1100

    + add more utility methods for constraints

--- UIViewChainable/Classes/UIViewChainable.swift
+++ UIViewChainable/Classes/UIViewChainable.swift
@@ -19,12 +19,67 @@ public extension UIViewChainable where Self: UIView {
         layout(self)
         return self
     }
+    
+    
+    //Automatically turn off the auto sizing flag
+    @discardableResult
+    public func constraintLayout(_ layout: (Self) -> Void) -> Self {
+        translatesAutoresizingMaskIntoConstraints = false
+        return self.layout(layout)
+    }
+    
+    //Activate all the constraints
+    //You should only call this method when all the constraints are programatically added and should be all activated
+    //Do not use this method if you have conditional constraints
+    @discardableResult
+    public func activateAllConstraints() -> Self {
+        return activate(constraints)
+    }
+    
+    @discardableResult
+    public func activate(_ constraints:[NSLayoutConstraint]) -> Self {
+        constraints.forEach {
+            $0.activate()
+        }
+        
+        return self
+    }
+    
+    @discardableResult
+    public func deactivateAllConstraints() -> Self {
+        return deactivate(constraints)
+    }
+    
+    @discardableResult
+    public func deactivate(_ constraints:[NSLayoutConstraint]) -> Self {
+        constraints.forEach {
+            $0.deactivate()
+        }
+        
+        return self
+    }
 }
 
 extension UIView: UIViewChainable {
     @discardableResult
     public func attachTo(_ superView: UIView) -> Self {
         superView.addSubview(self)
+        return self
+    }
+}
+
+extension NSLayoutConstraint {
+    @discardableResult
+    public func activate() -> Self{
+        isActive = true
+        
+        return self
+    }
+    
+    @discardableResult
+    public func deactivate() -> Self{
+        isActive = false
+        
         return self
     }
 }
